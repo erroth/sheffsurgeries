21c2dc909ae9535cea7c681aa78916dfeb80d7cb
b7029763@my.shu.ac.uk <b7029763@my.shu.ac.uk>
2019-03-19 19:30:52 +0000

    Created Bootstrap, Added first data as a test

--- appointmentsystem/grails-app/init/appointmentsystem/BootStrap.groovy
+++ appointmentsystem/grails-app/init/appointmentsystem/BootStrap.groovy
@@ -1,8 +1,20 @@
-package appointmentsystem
+package com.thandley
 
 class BootStrap {
 
     def init = { servletContext ->
+
+def Tom = new Doctor (
+
+doctorName: 'Tom Handley',
+qualifications: 'PHD',
+position: 'GP',
+doctorEmail: 'thomas.handley@hotmail.com',
+password: 'yeet',
+doctorOffice: '9151',
+doctorPhone: '07530599239',
+bio: 'yeet'
+).save()
     }
     def destroy = {
     }
